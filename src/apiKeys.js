987c09f95eb45e2b9af1938763b1ad95862a3a89
Pranaya Chavan <126591961+Pranaya211@users.noreply.github.com>
2023-12-20 15:15:59 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "71143357f47898007ad0ec9310d90f03",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
